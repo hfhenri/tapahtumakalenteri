b68950fb43ff7039a49daa6a1e00d4adf2c03a61
hfhenri <101591901+hfhenri@users.noreply.github.com>
2025-01-31 14:57:26 +0200

    Create initial database schema

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Users (
+    UserID CHAR(32) PRIMARY KEY,
+    Username VARCHAR(50) UNIQUE NOT NULL,
+    PasswordHash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Events (
+    EventID CHAR(32) PRIMARY KEY,
+    UserID CHAR(32) NOT NULL,
+    Title VARCHAR(100) NOT NULL,
+    Description TEXT,
+    ShortDescription TEXT,
+    Price DECIMAL(10, 2) DEFAULT 0.00,
+    CategoryID INT,
+    ImageID CHAR(32),
+    Date TEXT,
+    
+    FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE,
+);
+
+CREATE TABLE EventImages (
+    ImageID CHAR(32) PRIMARY KEY,
+    ImageData BLOB
+
+    FOREIGN KEY (ImageID) REFERENCES Events(ImageID) ON DELETE CASCADE
+);
